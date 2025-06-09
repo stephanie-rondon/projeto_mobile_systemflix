6c817751effc00df625d63122a258450b979ff91
stephanie-rondon <stephanie.rondon@escola.pr.gov.br>
2025-06-09 09:46:05 -0300

    Comecei a colocar os códigos dentro do javascript

--- pages/Login/login.js
+++ pages/Login/login.js
@@ -1,19 +1,9 @@
-//Código criado por Chat GPT
+// Página de Stephanie
+'use strict'
 
-    const botaoEntrar = document.getElementById("btnEntrar");
-    botaoEntrar.addEventListener("click", function (event) {
-        event.preventDefault();
-        const emailDigitado = document.getElementById("e-mail").value;
-        const senhaDigitada = document.getElementById("senha").value;
-        const dadosUsuario = JSON.parse(localStorage.getItem("usuario"));
-        if (
-            dadosUsuario &&
-            emailDigitado === dadosUsuario.email &&
-            senhaDigitada === dadosUsuario.senha
-        ) {
-            window.location.href= "../page2/index2.html";
-        }else{
-            alert("E-mail ou senha incorretos.")
-        }
-    });
-
+async function getTasks(){
+    const url = 'http://localhost:3000/cadastro'
+    const response = await fetch(url)
+    const tasks = await response.json()
+    console.log (tasks)
+}
