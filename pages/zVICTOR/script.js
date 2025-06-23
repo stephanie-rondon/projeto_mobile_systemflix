0b6cb0b36ee894e580cf58d963444a053d088212
bielqk08 <victorgabrielcr8@gmail.com>
2025-06-23 12:21:28 -0300

    remodelei meu site

--- pages/zVICTOR/script.js
+++ pages/zVICTOR/script.js
@@ -1,23 +1,8 @@
-let currentIndex = 0;
-const items = document.querySelectorAll('.item');
-const totalItems = items.length;
-const carousel = document.querySelector('.carousel');
-
-function updateCarousel() {
-    const offset = -currentIndex * (items[0].offsetWidth + 20); // 20 = margem entre itens
-    carousel.style.transform = `translateX(${offset}px)`;
-}
-
-document.querySelector('.arrow.left').addEventListener('click', () => {
-    if (currentIndex > 0) {
-        currentIndex--;
-        updateCarousel();
-    }
-});
-
-document.querySelector('.arrow.right').addEventListener('click', () => {
-    if (currentIndex < totalItems - 3) { // Exibe 3 itens por vez
-        currentIndex++;
-        updateCarousel();
-    }
-});
+function salvar() {
+    alert("Perfil salvo!");
+  }
+  
+  function cancelar() {
+    alert("Edição cancelada.");
+  }
+  
